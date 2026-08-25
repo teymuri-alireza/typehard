b3bda57692010bb71dc95e2fe089e7bb6843e911
teymuri-alireza <teymuri.alireza98@gmail.com>
2026-08-25 13:54:56 +0330

    feat: add array of lessons

---
+++ src/lessons/allLessons.ts
@@ -0,0 +1,82 @@
+import type { TypingLesson } from "../types/session.js";
+
+
+export const lessons: TypingLesson[] = [
+    {
+        "id": "beginner-001",
+        "title": "The Jungle Book",
+        "difficulty": "beginner",
+        "text": `and he grew and grew strong as a boy must grow who does
+not know that he is learning any lessons and who has nothing in the
+world to think of except things to eat.`,
+        "author": "Rudyard Kipling",
+        "source": "https://www.gutenberg.org/cache/epub/236/pg236-images.html"
+    },
+    {
+        "id": "beginner-002",
+        "title": "",
+        "difficulty": "beginner",
+        "text": `placeholder 2`
+    },
+    {
+        "id": "beginner-003",
+        "title": "",
+        "difficulty": "beginner",
+        "text": `placeholder 3`
+    },
+    {
+        "id": "intermediate-001",
+        "title": "What Is Art?",
+        "difficulty": "intermediate",
+        "text": `Take up any one of our ordinary newspapers, and you will
+find a part devoted to the theatre and music. In almost every number you
+will find a description of some art exhibition, or of some particular
+picture, and you will always find reviews of new works of art that have
+appeared, of volumes of poems, of short stories, or of novels.`,
+        "author": "Leo Tolstoy",
+        "source": "https://www.gutenberg.org/cache/epub/64908/pg64908-images.html"
+
+    },
+    {
+        "id": "intermediate-002",
+        "title": "Alice's Adventures in Wonderland",
+        "difficulty": "intermediate",
+        "text": `Alice was beginning to get very tired of sitting by her
+sister on the bank, and of having nothing to do: once or twice she had
+peeped into the book her sister was reading, but it had no pictures or
+conversations in it, "and what is the use of a book," thought Alice
+"without pictures or conversations?"`,
+        "author": "Lewis Carroll",
+        "source": "https://www.gutenberg.org/cache/epub/11/pg11-images.html"
+    },
+    {
+        "id": "intermediate-003",
+        "title": "The Empty House and Other Ghost Stories",
+        "difficulty": "intermediate",
+        "text": `There was manifestly nothing in the external appearance of
+this particular house to bear out the tales of the horror that was said to
+reign within. It was neither lonely nor unkempt. It stood, crowded into a
+corner of the square, and looked exactly like the houses on either side of
+it.`,
+        "author": "Algernon Blackwood",
+        "source": "https://www.gutenberg.org/cache/epub/14471/pg14471-images.html"
+    },
+    {
+        "id": "advanced-001",
+        "title": "",
+        "difficulty": "advanced",
+        "text": `placeholder 7`
+    },
+    {
+        "id": "advanced-002",
+        "title": "",
+        "difficulty": "advanced",
+        "text": `placeholder 8`
+    },
+    {
+        "id": "advanced-003",
+        "title": "",
+        "difficulty": "advanced",
+        "text": `placeholder 9`
+    },
+];
