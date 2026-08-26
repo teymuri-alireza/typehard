fa746656a52ea28bf1f5adcd0ed9cc18aa300106
teymuri-alireza <teymuri.alireza98@gmail.com>
2026-08-26 15:35:08 +0330

    chore: add more lessons and remove redundant new lines

--- src/lessons/allLessons.ts
+++ src/lessons/allLessons.ts
@@ -6,9 +6,7 @@ export const lessons: TypingLesson[] = [
         "id": "beginner-001",
         "title": "The Jungle Book",
         "difficulty": "beginner",
-        "text": `and he grew and grew strong as a boy must grow who does
-not know that he is learning any lessons and who has nothing in the
-world to think of except things to eat.`,
+        "text": `and he grew and grew strong as a boy must grow who does not know that he is learning any lessons and who has nothing in the world to think of except things to eat.`,
         "author": "Rudyard Kipling",
         "source": "https://www.gutenberg.org/cache/epub/236/pg236-images.html"
     },
@@ -28,11 +26,7 @@ world to think of except things to eat.`,
         "id": "intermediate-001",
         "title": "What Is Art?",
         "difficulty": "intermediate",
-        "text": `Take up any one of our ordinary newspapers, and you will
-find a part devoted to the theatre and music. In almost every number you
-will find a description of some art exhibition, or of some particular
-picture, and you will always find reviews of new works of art that have
-appeared, of volumes of poems, of short stories, or of novels.`,
+        "text": `Take up any one of our ordinary newspapers, and you will find a part devoted to the theatre and music. In almost every number you will find a description of some art exhibition, or of some particular picture, and you will always find reviews of new works of art that have appeared, of volumes of poems, of short stories, or of novels.`,
         "author": "Leo Tolstoy",
         "source": "https://www.gutenberg.org/cache/epub/64908/pg64908-images.html"
 
@@ -41,11 +35,7 @@ appeared, of volumes of poems, of short stories, or of novels.`,
         "id": "intermediate-002",
         "title": "Alice's Adventures in Wonderland",
         "difficulty": "intermediate",
-        "text": `Alice was beginning to get very tired of sitting by her
-sister on the bank, and of having nothing to do: once or twice she had
-peeped into the book her sister was reading, but it had no pictures or
-conversations in it, "and what is the use of a book," thought Alice
-"without pictures or conversations?"`,
+        "text": `Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, "and what is the use of a book," thought Alice "without pictures or conversations?"`,
         "author": "Lewis Carroll",
         "source": "https://www.gutenberg.org/cache/epub/11/pg11-images.html"
     },
@@ -53,25 +43,33 @@ conversations in it, "and what is the use of a book," thought Alice
         "id": "intermediate-003",
         "title": "The Empty House and Other Ghost Stories",
         "difficulty": "intermediate",
-        "text": `There was manifestly nothing in the external appearance of
-this particular house to bear out the tales of the horror that was said to
-reign within. It was neither lonely nor unkempt. It stood, crowded into a
-corner of the square, and looked exactly like the houses on either side of
-it.`,
+        "text": `There was manifestly nothing in the external appearance of this particular house to bear out the tales of the horror that was said to reign within. It was neither lonely nor unkempt. It stood, crowded into a corner of the square, and looked exactly like the houses on either side of it.`,
         "author": "Algernon Blackwood",
         "source": "https://www.gutenberg.org/cache/epub/14471/pg14471-images.html"
     },
     {
+        "id": "intermediate-004",
+        "title": "The Blue Lagoon: A Romance",
+        "difficulty": "intermediate",
+        "text": `He fed the children hurriedly with some biscuits and tinned meat, and then, with a biscuit in his hand, eating as he went, he trotted about the decks, collecting things and stowing them in the dinghy. The bolt of striped flannel, all the old clothes, a housewife full of needles and thread, such as seamen sometimes carry, the half-sack of potatoes, a saw which he found in the caboose, the precious coil of tobacco, and a lot of other odds and ends he transhipped, sinking the little dinghy several strakes in the process.`,
+        "author": "H. De Vere Stacpoole",
+        "source": "https://www.gutenberg.org/cache/epub/393/pg393-images.html"
+    },
+    {
         "id": "advanced-001",
-        "title": "",
+        "title": "The University of Michigan",
         "difficulty": "advanced",
-        "text": `placeholder 7`
+        "text": `It is 168 feet in all across the front and 233 feet deep, with four stories, a basement, and sub-basement. In addition to other usual facilities of a large club, it contains a swimming pool (not completed in 1920), a bowling alley, an immensely popular cafeteria for men, known as the Tap-Room, a woman's dining-room with a separate entrance, a billiard room, with twenty-five tables, a large banquet and assembly hall, 58 by 104 feet, for dinners, dances, and large gatherings.`,
+        "author": "Wilfred Byron Shaw",
+        "source": "https://www.gutenberg.org/cache/epub/21532/pg21532-images.html"
     },
     {
         "id": "advanced-002",
-        "title": "",
+        "title": "Please stand by",
         "difficulty": "advanced",
-        "text": `placeholder 8`
+        "text": `Dr. Hillary and Henry Caddock came in from a late game of billiards at the Cardinal Valley Club down town. "Smoke another cigar before you go up, professor?" asked Caddock, stepping just inside the parlor door. "When I'm up as late as this, I don't care when I go to bed." "Thanks, no; cigars are a little heavy for me after the first one. And, as for its being late, I shall write a little on the new chapter of my book before I turn in." "Hello!" said Caddock, catching sight of the dejected figure in the chair. "Oh, it's Raymond. Hm-heard you on the air tonight, Raymond. Pretty good! Is the position permanent, may I ask? If so, I congratulate you, I'm sure."`
+        ,"author": "Joseph Ivers Lawrence",
+        "source": "https://www.gutenberg.org/cache/epub/79452/pg79452-images.html"
     },
     {
         "id": "advanced-003",
