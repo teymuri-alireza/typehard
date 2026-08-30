1e5aeda847a6402b97b18d4faa1e24de4cc7fa59
teymuri-alireza <teymuri.alireza98@gmail.com>
2026-08-30 14:07:04 +0330

    feat(ui): add dark yellow and dark pink themes

--- src/settings/theme.ts
+++ src/settings/theme.ts
@@ -1,7 +1,7 @@
-export type ThemeType = "light" | "dark";
+export type ThemeType = "light" | "dark" | "dark_yellow" | "dark_pink";
 
 export function applyTheme(theme: ThemeType): void {
-    document.documentElement.classList.remove("light", "dark");
+    document.documentElement.classList.remove("light", "dark", "dark_yellow", "dark_pink");
 
     document.documentElement.classList.add(theme);
 }
