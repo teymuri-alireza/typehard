6534fa5252a92d7d14459e7bb99e09982cd71b8f
teymuri-alireza <teymuri.alireza98@gmail.com>
2026-09-05 12:49:25 +0330

    feat(theme): add three new themes
    Added brown vintage, summer sea, and green vintage.

--- src/settings/theme.ts
+++ src/settings/theme.ts
@@ -1,7 +1,15 @@
-export type ThemeType = "light" | "light_blue" | "dark" | "dark_yellow" | "dark_pink" | "green_nature" | "warm_purple";
+export type ThemeType = 
+    | "light" | "light_blue"
+    | "dark" | "dark_yellow"
+    | "dark_pink" | "green_nature"
+    | "warm_purple" | "brown_vintage"
+    | "summer_sea" | "green_vintage";
 
 export function applyTheme(theme: ThemeType): void {
-    document.documentElement.classList.remove("light", "light_blue", "dark", "dark_yellow", "dark_pink", "green_nature", "warm_purple");
+    document.documentElement.classList.remove(
+        "light", "light_blue", "dark", "dark_yellow", "dark_pink", "green_nature", "warm_purple", "brown_vintage", "summer_sea",
+        "green_vintage"
+    );
 
     document.documentElement.classList.add(theme);
 }
